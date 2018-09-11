ead7811d0aab22182250aaa17fe42fb970046f74
dawid-kropidlowski <dawid.kropidlowski@gmail.com>
2018-09-11 17:36:13 +0200

    Retrieving file with daily quotes from Bossa.pl

--- webinterface.py
+++ webinterface.py
@@ -1,10 +1,26 @@
 from urllib import request
 
-data = request.urlopen('http://bossa.pl/pub/metastock/mstock/sesjaall/sesjaall.prn')
+cnfg = {}
 
+# open configuration file
+config = open('config.txt', 'r')
 
-for line in data:
+# load configuration
+for line in config:
+    item = line.split('=')
+    cnfg[item[0].replace(' ', '')] = item[1].replace('\n','')
+
+print(cnfg)
+
+last_day_close_url = cnfg['last_day_close_url']
+
+# open url with data from last day close of stock market
+last_day_close_data = request.urlopen(last_day_close_url)
+
+# read the file
+for line in last_day_close_data:
+    # decoding data as they are seen by Python as binary
     daily_quotes = line.decode()
     daily_quotes = daily_quotes.split(',')
-    daily_quotes = ' '.join(daily_quotes)
-    print(daily_quotes)
+#    daily_quotes = ' '.join(daily_quotes)
+#    print(daily_quotes)
